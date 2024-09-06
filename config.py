bedd2cf2c1347f1969c7e585d76db5e97602b366
ARJUN70380 <prince70380@gmail.com>
2024-09-06 10:40:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7119472649:AAHBsiP2Byg1Eix-tzrBIH6DEhRtu6RHYYE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("arjun70380")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1135761619")
 
 
 
